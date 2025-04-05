b856e3e040a530eccff328456582abc6f6899ac0
Vinícius César Bertolo Calligaris <vinicius.cesar7@hotmail.com>
2025-04-05 11:30:17 -0300

    Finalizado

--- calculadora.v
+++ calculadora.v
@@ -5,6 +5,18 @@ module calculadora(
   output  [7:0] saida
 );
 
-// insira seu código aqui
+  assign saida = 
+  
+  (3'b000 == codigo) ? 8'b00000000:
+
+  (3'b001 == codigo) ? entrada_A: 
+
+  (3'b010 == codigo) ? entrada_B:
+
+  (3'b011 == codigo) ? entrada_A + entrada_B: 
+
+  (3'b100 == codigo) ? entrada_A - entrada_B:
+
+  8'b00000000;  
 
 endmodule
